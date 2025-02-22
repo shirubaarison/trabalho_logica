07fbf8b74494b20415e6e27bdcf44523523fdf7c
Alysson Silva <shirubaarison@gmail.com>
2025-02-22 16:52:15 -0300

    refatoração

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-from codigo import Instancia
+import Instancia
+import utils_grafico as gra
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 def processar(a, n, k, qtd_instancias):
@@ -42,59 +43,22 @@ def executar(k: int, qtd_instancias, a_lim, valores_n):
   return resultados
 
 
-def construir_grafico_prob(k: int, resultados, valores_n):
-  plt.figure(figsize=(12, 8))
-  for n in valores_n:
-    alphas = []  # alpha
-    probs = []  # prob de satisfazibilidade
-    for a in sorted(resultados.keys()):
-        alphas.append(a)
-        probs.append(resultados[a][n]['prob'])
-    plt.plot(alphas, probs, label=f'n = {n}')
-
-  plt.xlabel('Alpha (α)')
-  plt.ylabel('Probabilidade (%)')
-  titulo = f'{k}-SAT Satisfazibilidade vs α'
-  plt.title(titulo)
-  plt.legend()
-  plt.grid(True)
-
-  caminho = f'graficos/{k}sat.png'
-  plt.savefig(caminho)
-  print(f'Gráfico de probabilidade gerado em: {caminho}')
-
-
-def construir_grafico_tempo(k: int, resultados, valores_n):
-  plt.figure(figsize=(12, 8))
-  for n in valores_n:
-    alphas = []  # a
-    tempos = []  # tempo
-    for a in sorted(resultados.keys()):
-      if n in resultados[a]:
-        alphas.append(a)
-        tempos.append(resultados[a][n]['tempo'])
-
-    plt.plot(alphas, tempos, label=f'n = {n}')
-
-  plt.xlabel('Alpha (α)')
-  plt.ylabel('Tempo de Resolução (s)')
-  titulo = f'{k}-SAT Tempo de Resolução vs α'
-  plt.title(titulo)
-  plt.legend()
-  plt.grid(True)
-
-  caminho = f'graficos/{k}sat_tempo.png'
-  plt.savefig(caminho)
-  print(f'Gráfico de tempo gerado em: {caminho}')
-
-
 def salvar_resultado(resultado, k: int):
   caminho = f'data/{k}_sat_result.json'
 
   with open(caminho, 'w') as file:
      json.dump(resultado, file)
   print(f"Resultados salvos em: {caminho}")
-     
+
+
+def ler_resultados(k: int):
+  caminho = f'data/{k}_sat_result.json'
+  with open(caminho, 'r') as file:
+    resultado = json.load(file)
+
+    resultado = {float(a): {int(n): v for n, v in subdict.items()} for a, subdict in resultado.items()}
+  
+  return resultado
 
 def main():
   # Tamanhos de n
@@ -105,23 +69,27 @@ def main():
   a_3 = 10
   a_5 = 30
 
-  qtd_instancias = 30
-  # Executar dois processos um para 3-SAT e outro para 5-SAT ao mesmo tempo
-  with ProcessPoolExecutor(max_workers=2) as executor:
-    futures = []
-    futures.append(executor.submit(executar, 3, qtd_instancias, a_3, n_instancias_3))
-    futures.append(executor.submit(executar, 5, qtd_instancias, a_5, n_instancias_5))
+  # qtd_instancias = 30
+  # # Executar dois processos um para 3-SAT e outro para 5-SAT ao mesmo tempo
+  # with ProcessPoolExecutor(max_workers=2) as executor:
+  #   futures = []
+  #   futures.append(executor.submit(executar, 3, qtd_instancias, a_3, n_instancias_3))
+  #   futures.append(executor.submit(executar, 5, qtd_instancias, a_5, n_instancias_5))
 
-    for future in as_completed(futures):
-      resultados = future.result()
-      if future == futures[0]:        # 0 é os 3-SAT
-        salvar_resultado(resultados, 3)
-        construir_grafico_prob(3, resultados, n_instancias_3)
-        construir_grafico_tempo(3, resultados, n_instancias_3)
-      else:
-        salvar_resultado(resultados, 5)
-        construir_grafico_prob(5, resultados, n_instancias_5)
-        construir_grafico_tempo(5, resultados, n_instancias_5)
+  #   for future in as_completed(futures):
+  #     resultados = future.result()
+  #     if future == futures[0]:        # 0 é os 3-SAT
+  #       salvar_resultado(resultados, 3)
+  #       gra.construir_grafico_prob(3, resultados, n_instancias_3)
+  #       gra.construir_grafico_tempo(3, resultados, n_instancias_3)
+  #     else:
+  #       salvar_resultado(resultados, 5)
+  #       gra.construir_grafico_prob(5, resultados, n_instancias_5)
+  #       gra.construir_grafico_tempo(5, resultados, n_instancias_5)
+  resultados = ler_resultados(3)
+  
+  gra.construir_grafico_prob(3, resultados, n_instancias_3)
+  gra.construir_grafico_tempo(3, resultados, n_instancias_3)
 
 
 if __name__ == "__main__":
